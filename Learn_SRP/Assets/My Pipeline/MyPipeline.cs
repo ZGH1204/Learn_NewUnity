9d6fce8e316e19483f4a369b1337f00a975f2b46
zhaoguanghui <972833647@qq.com>
2019-11-17 21:54:43 +0800

    Init

---
+++ Learn_SRP/Assets/My Pipeline/MyPipeline.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+using Conditional = System.Diagnostics.ConditionalAttribute;
+
+public class MyPipeline : RenderPipeline
+{
+    CullResults cull;
+    CommandBuffer cmd = new CommandBuffer() { name = "保持空杯心态 " };
+
+    Material errorMaterial;
+
+    public override void Dispose()
+    {
+        base.Dispose();
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+
+        foreach (var camera in cameras)
+        {
+            Render(renderContext, camera);
+        }
+    }
+
+    void Render(ScriptableRenderContext renderContext, Camera camera)
+    {
+        renderContext.SetupCameraProperties(camera);
+
+        // 裁剪
+        ScriptableCullingParameters cullingParameters;
+        if (!CullResults.GetCullingParameters(camera, out cullingParameters))
+        {
+            return;
+        }
+#if UNITY_EDITOR
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+#endif
+        CullResults.Cull(ref cullingParameters, renderContext, ref cull);
+
+
+        CameraClearFlags clearFlags = camera.clearFlags;
+
+        // 清除
+        cmd.ClearRenderTarget((clearFlags & CameraClearFlags.Depth) != 0, (clearFlags & CameraClearFlags.Color) != 0, camera.backgroundColor);
+        cmd.BeginSample("Render Camera");
+        renderContext.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+
+        // 绘制不透明几何体
+        var drawSettings = new DrawRendererSettings(camera, new ShaderPassName("SRPDefaultUnlit"));
+        drawSettings.sorting.flags = SortFlags.CommonOpaque;
+        var filterSettings = new FilterRenderersSettings(true) { renderQueueRange = RenderQueueRange.opaque };
+        renderContext.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings
+        );
+
+        // 绘制天空盒
+        renderContext.DrawSkybox(camera);
+
+        // 绘制半透明几何体
+        drawSettings.sorting.flags = SortFlags.CommonTransparent;
+        filterSettings.renderQueueRange = RenderQueueRange.transparent;
+        renderContext.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings
+        );
+
+        DrawDefaultPipeline(renderContext, camera);
+
+        cmd.EndSample("Render Camera");
+        renderContext.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+
+        renderContext.Submit();
+    }
+
+    [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+    void DrawDefaultPipeline(ScriptableRenderContext context, Camera camera)
+    {
+        if (errorMaterial == null)
+        {
+            Shader errorShader = Shader.Find("Hidden/InternalErrorShader");
+            errorMaterial = new Material(errorShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+
+        var drawSettings = new DrawRendererSettings(
+            camera, new ShaderPassName("ForwardBase")
+        );
+        drawSettings.SetShaderPassName(1, new ShaderPassName("PrepassBase"));
+        drawSettings.SetShaderPassName(2, new ShaderPassName("Always"));
+        drawSettings.SetShaderPassName(3, new ShaderPassName("Vertex"));
+        drawSettings.SetShaderPassName(4, new ShaderPassName("VertexLMRGBM"));
+        drawSettings.SetShaderPassName(5, new ShaderPassName("VertexLM"));
+        drawSettings.SetOverrideMaterial(errorMaterial, 0);
+
+        var filterSettings = new FilterRenderersSettings(true);
+
+        context.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings
+        );
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
